66998ee353324080c0946d24e287035fe7b693e4
Percy Amarasinghe <dvpamarasinghe31@gmail.com>
2022-03-08 19:28:03 +0530

    Add About Us

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -13,3 +13,25 @@ typewriter
   .typeString("WELCOME TO <strong style='color:#000;'>OFFICIAL SITE OF ST.ANTHONY'S COLLEGE</strong>")
   .pauseFor(1000)
   .start();
+
+  //purecounter
+  new PureCounter();
+
+new PureCounter({
+ 
+    selector: '.purecounter',	
+
+  
+    start: 0, 			            
+    end: 100, 			            
+    duration: 2, 	                
+    delay: 10, 			            
+    once: true, 		           
+    repeat: false, 		            
+    decimals: 0, 		           
+    legacy: true,                   
+    filesizing: false, 	           
+    currency: false, 	            
+    separator: false, 	          
+});
+//purecounter
